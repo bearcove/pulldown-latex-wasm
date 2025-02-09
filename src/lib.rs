725ed9948a3ea4749ab5f033305cbe9b3935d453
Amos Wenger <amos@bearcove.net>
2025-02-09 14:26:22 +0100

    Well it builds to _something_

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use pulldown_latex::config::DisplayMode;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+fn convert_math_to_mathml(input: &str, display_mode: DisplayMode) -> String {
+    let mut output = String::new();
+    let storage = pulldown_latex::Storage::new();
+    let parser = pulldown_latex::Parser::new(input, &storage);
+    pulldown_latex::push_mathml(
+        &mut output,
+        parser,
+        pulldown_latex::RenderConfig {
+            display_mode,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+    output
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn display_math_to_mathml(input: &str) -> String {
+    convert_math_to_mathml(input, DisplayMode::Block)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn inline_math_to_mathml(input: &str) -> String {
+    convert_math_to_mathml(input, DisplayMode::Inline)
 }
